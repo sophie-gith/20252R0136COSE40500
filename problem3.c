9f55012ebc0c7813a200345a2da707741e450803
sophie-gith <sophialesydney@gmail.com>
2025-12-19 13:54:31 +0900

    Add files via upload

--- problem3.c
+++ problem3.c
@@ -1,20 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-long long get_power(int b, int e)
-{
-    if (e <= 0)
-        return 1;
-    else
-        return (long long)b * get_power(b, e - 1);
-}
+#define CLIP(x, lo, hi) ( (((x) >= (lo)) && ((x) <= (hi))) ? (x) : ( ((x) < (lo)) ? (lo) : (hi) ) )
 
-int main(void)
+int main(void) 
 {
-    int base, exp;
-    printf("Enter base and exponent: ");
-    scanf("%d %d", &base, &exp);
-
-    printf("Result = %lld", get_power(base, exp));
+    int val, min, max;
+    printf("Enter value, min, and max: ");
+    scanf("%d %d %d", &val, &min, &max);
+    printf("Clipped = %d", CLIP(val, min, max));
     return 0;
 }
