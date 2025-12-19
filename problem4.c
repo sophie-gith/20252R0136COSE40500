9329d23a2d651b34066c50316139c5fee5ce2b3b
sophie-gith <sophialesydney@gmail.com>
2025-12-19 13:54:40 +0900

    Add files via upload

--- problem4.c
+++ problem4.c
@@ -1,23 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int digit_sum(int x)
-{
-    if (x <= 0)
-        return 0;
-    else
-        return (x % 10) + digit_sum(x / 10);
-}
+#define SWAP(x, y, temp) {(temp) = (x);\
+                                   (x) = (y);\
+                                   (y) = (temp);}
 
 int main(void)
 {
-    int n;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
+    int a, b, temp;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
 
-    if (n < 0)
-        n = -n;
+    SWAP(a, b, temp);
 
-    printf("Sum of digits = %d", digit_sum(n));
+    printf("Swapped = %d %d", a, b);
     return 0;
 }
